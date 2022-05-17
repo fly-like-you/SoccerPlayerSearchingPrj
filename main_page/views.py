9b867959429d79012554ed7a9133077faaef18f9
fly_like_you <qkrwnsgh71w@gmail.com>
2022-05-17 22:16:57 +0900

    FBW로 선수 카드 만들기

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Player
 # Create your views here.
+def index(request):
+    player = Player.objects.all().order_by('-name')
+    return render(
+        request,
+        'main_page/index.html',
+        {
+            'player': player,
+        }
+    )
