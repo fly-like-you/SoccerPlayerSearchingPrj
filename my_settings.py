c1f7e4eb139a39e076cd4c87fe80770ed614b92b
fly_like_you <qkrwnsgh71w@gmail.com>
2022-05-17 15:17:01 +0900

    Django프로젝트 생성

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', #1
+        'NAME': 'DEPPODB', #2
+        'USER': 'deppoDB', #3
+        'PASSWORD': 'DEPPODB!!!q1w2e3r4',  #4
+        'HOST': '203.255.57.227',   #5
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = 'django-insecure-9hh7)3muc5^+u@_)7f-==dx1s1hmjbn9j3+1pe^%h&5n(88=i9'
