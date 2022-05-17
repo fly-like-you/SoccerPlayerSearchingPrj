bbc9689dd9e60a91e46f0780b642e1ae0b60b7fe
fly_like_you <qkrwnsgh71w@gmail.com>
2022-05-17 20:56:12 +0900

    Player 모델 생성

--- main_page/models.py
+++ main_page/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Player(models.Model):
+    name = models.CharField(max_length=10)
+    position = models.CharField(max_length=5)
+    team_name = models.CharField(max_length=30)
+    pass_success_rate = models.IntegerField()
+    shoot_success_rate = models.IntegerField()
+
+    def __str__(self):
+        return f'[{self.pk}] {self.name}'
 # Create your models here.
--- soccer_django_prj/settings.py
+++ soccer_django_prj/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_page',
+    'single_pages'
 ]
 
 MIDDLEWARE = [
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
