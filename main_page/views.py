3d919769756283a463d153065a75477c789035dc
fly_like_you <qkrwnsgh71w@gmail.com>
2022-05-20 09:25:43 +0900

    팀 카테고리 추가

--- main_page/views.py
+++ main_page/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from .models import Player
+from .models import Player, Category
 from django.views.generic import ListView, DetailView
+
 
 #CBV 방식
 class CardList(ListView): # FBV방식의 index를 대체하는 클래스
     model = Player
     ordering = '-name'
+
+    def get_context_data(self, **kwargs):
+        context = super(CardList, self).get_context_data()  #오버라이딩
+        context['categories'] = Category.objects.all() # 모든 카테고리를 가져와 키에 연결해담음
+        context['no_category_card_count'] = Player.objects.filter(category=None).count() #카테고리가 지정되지않은 선수를 키에 담는다
+
+        return context
+
 
 class CardDetail(DetailView):
     model = Player
